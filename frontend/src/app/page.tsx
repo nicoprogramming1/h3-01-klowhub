b4aa5c88c607f9e8864d787f623e6008acade0c9
Stiwii <stiwers1997@gmail.com>
2024-11-14 01:12:03 -0500

    SS: aderir colores y botones

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -4,11 +4,22 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center gap-4 py-20 px-10">
       <h1 className="text-green-400">KlowHub</h1>
-      <div>
-        <Button>Primary</Button>
-        <Button variant="secondary">Secondary</Button>
-        <Button variant="destructive">Destructive</Button>
-        <Button variant="outline">Outline</Button>
+      <div className="flex gap-x-2">
+        <Button variant={"primario"}>Primary</Button>
+        <Button variant={"outline"} size={"lg"}>
+          Outline
+        </Button>
+        <Button size={"sm"} variant={"destructive"}>
+          Destructive
+        </Button>
+        <Button variant="link" size="icon">
+          P
+        </Button>
+        <Button variant="secudario">Secundario</Button>
+        {/* Este se podría hacer un componente para el botton Back separado  */}
+        <Button variant="outline" className="">
+          A
+        </Button>
       </div>
     </div>
   );
--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -10,55 +10,81 @@ export default {
     "./src/features/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        background: "hsl(var(--background))",
-        foreground: "hsl(var(--foreground))",
-        card: {
-          DEFAULT: "hsl(var(--card))",
-          foreground: "hsl(var(--card-foreground))",
-        },
-        popover: {
-          DEFAULT: "hsl(var(--popover))",
-          foreground: "hsl(var(--popover-foreground))",
-        },
-        primary: {
-          DEFAULT: "hsl(var(--primary))",
-          foreground: "hsl(var(--primary-foreground))",
-        },
-        secondary: {
-          DEFAULT: "hsl(var(--secondary))",
-          foreground: "hsl(var(--secondary-foreground))",
-        },
-        muted: {
-          DEFAULT: "hsl(var(--muted))",
-          foreground: "hsl(var(--muted-foreground))",
-        },
-        accent: {
-          DEFAULT: "hsl(var(--accent))",
-          foreground: "hsl(var(--accent-foreground))",
-        },
-        destructive: {
-          DEFAULT: "hsl(var(--destructive))",
-          foreground: "hsl(var(--destructive-foreground))",
-        },
-        border: "hsl(var(--border))",
-        input: "hsl(var(--input))",
-        ring: "hsl(var(--ring))",
-        chart: {
-          "1": "hsl(var(--chart-1))",
-          "2": "hsl(var(--chart-2))",
-          "3": "hsl(var(--chart-3))",
-          "4": "hsl(var(--chart-4))",
-          "5": "hsl(var(--chart-5))",
-        },
-      },
-      borderRadius: {
-        lg: "var(--radius)",
-        md: "calc(var(--radius) - 2px)",
-        sm: "calc(var(--radius) - 4px)",
-      },
-    },
+  	extend: {
+  		colors: {
+  			background: 'hsl(var(--background))',
+  			foreground: 'hsl(var(--foreground))',
+  			card: {
+  				DEFAULT: 'hsl(var(--card))',
+  				foreground: 'hsl(var(--card-foreground))'
+  			},
+  			popover: {
+  				DEFAULT: 'hsl(var(--popover))',
+  				foreground: 'hsl(var(--popover-foreground))'
+  			},
+  			primary: {
+  				DEFAULT: 'hsl(var(--primary))',
+  				foreground: 'hsl(var(--primary-foreground))'
+  			},
+  			primario: {
+  				'100': '#dfd1f3',
+  				'200': '#bfa3e7',
+  				'300': '#9f74dc',
+  				'400': '#9f74dc',
+  				'500': '#632daf',
+  				'600': '#532692',
+  				'700': '#421e75',
+  				'800': '#321758',
+  				'900': '#210f3a',
+  				DEFAULT: '#632daf',
+  				foreground: '#ffff'
+  			},
+  			secondary: {
+  				DEFAULT: 'hsl(var(--secondary))',
+  				foreground: 'hsl(var(--secondary-foreground))'
+  			},
+  			secundario: {
+  				'100': '#c0d6fb',
+  				'200': '#a1c2fa',
+  				'300': '#81aef8',
+  				'400': '#6299f6',
+  				'500': '#4285f4',
+  				'600': '#1061ea',
+  				'700': '#0a49b0',
+  				'800': '#083075',
+  				'900': '#03183b',
+  				DEFAULT: '#4285f4',
+  				foreground: '#ffff'
+  			},
+  			muted: {
+  				DEFAULT: 'hsl(var(--muted))',
+  				foreground: 'hsl(var(--muted-foreground))'
+  			},
+  			accent: {
+  				DEFAULT: 'hsl(var(--accent))',
+  				foreground: 'hsl(var(--accent-foreground))'
+  			},
+  			destructive: {
+  				DEFAULT: 'hsl(var(--destructive))',
+  				foreground: 'hsl(var(--destructive-foreground))'
+  			},
+  			border: 'hsl(var(--border))',
+  			input: 'hsl(var(--input))',
+  			ring: 'hsl(var(--ring))',
+  			chart: {
+  				'1': 'hsl(var(--chart-1))',
+  				'2': 'hsl(var(--chart-2))',
+  				'3': 'hsl(var(--chart-3))',
+  				'4': 'hsl(var(--chart-4))',
+  				'5': 'hsl(var(--chart-5))'
+  			}
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
   },
   plugins: [require("tailwindcss-animate")],
 } satisfies Config;
