eca5f8d2a2945d9ea106e530d75693a966697293
Abimael Junior Pacheco <122593014+Abimael-p@users.noreply.github.com>
2024-11-15 16:43:27 +0000

    configModel

--- backend/src/controllers/authController.ts
+++ backend/src/controllers/authController.ts
@@ -1,24 +1,26 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 import { Request, Response } from 'express';
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import UserModel from '../models/UserModel';
-import dotenv from 'dotenv';
-
-dotenv.config();
 
 export const register = async (req: Request, res: Response) => {
-  const { username, password } = req.body;
+  const { password, ...User } = req.body;
   const hashedPassword = await bcrypt.hash(password, 10);
-  const user = await UserModel.create({ username, password: hashedPassword });
+  const userData = await UserModel.create({ User, password: hashedPassword });
+  const  {password: _, ...user } = userData;
   res.json({ user });
 };
 
 export const login = async (req: Request, res: Response) => {
-  const { username, password } = req.body;
-  const user = await UserModel.findOne({ where: { username } });
-  if (user && (await bcrypt.compare(password, user.password))) {
-    const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET!, { expiresIn: '1h' });
-    res.json({ token });
+  const { email, password } = req.body;
+  const userData = await UserModel.findOne({ where: { email } });
+  if (userData && (await bcrypt.compare(password, userData.password))) {
+    const token = jwt.sign({ id: userData.id }, process.env.JWT_SECRET!, { expiresIn: '1h' });
+    const { password: _, ...user } = userData;
+    res.json({ user, token });
   } else {
     res.status(401).json({ message: 'Invalid credentials' });
   }
