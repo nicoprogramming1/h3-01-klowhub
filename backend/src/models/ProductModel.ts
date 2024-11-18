6cb53c3bd66e83494b4e1bdb699af87df3a38be1
wnorowsky <wnorowsky.nicolas@gmail.com>
2024-11-18 18:58:19 -0300

    Add Platform, Sector and Tags enumeration

--- backend/src/models/ProductModel.ts
+++ backend/src/models/ProductModel.ts
@@ -4,13 +4,28 @@ enum Tag {
   TAG1 = "Tag 1",
   TAG2 = "Tag 2",
   TAG3 = "Tag 3",
-  TAG4 = "Tag 4",
+}
+
+enum Platform {
+  APPSHEET = "AppSheet",
+  POWERAPPS = "PowerApps",
+}
+
+enum Sector {
+    ECOMMERCE = "Ecommerce",
+    INDUSTRY = "Industry",
+    EDUCATIONAL = "Educational",
+    MANAGEMENT = "Management",
+    TECHNOLOGY = "Technology"
 }
 
 export abstract class ProductModel extends Model {
   public id!: string;
   public title!: string;
   public detail!: string;
+  public platform!: Platform;
+  public image!: string
+  public sector!: Sector
   public tags!: Tag[];
   public price!: number;
   public ownerId!: string;
