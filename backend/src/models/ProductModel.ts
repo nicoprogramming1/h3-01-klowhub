d79fb1e4f46917ccb1a14a3eb8248b4070ac445a
nicoprogramming <wnorowsky.nicolas@gmail.com>
2024-11-19 19:59:43 -0300

    Move interface to new interface folder models/interfaces

--- backend/src/models/ProductModel.ts
+++ backend/src/models/ProductModel.ts
@@ -1,23 +1,5 @@
 import { Model } from "sequelize";
-
-enum Tag {
-  TAG1 = "Tag 1",
-  TAG2 = "Tag 2",
-  TAG3 = "Tag 3",
-}
-
-enum Platform {
-  APPSHEET = "AppSheet",
-  POWERAPPS = "PowerApps",
-}
-
-enum Sector {
-    ECOMMERCE = "Ecommerce",
-    INDUSTRY = "Industry",
-    EDUCATIONAL = "Educational",
-    MANAGEMENT = "Management",
-    TECHNOLOGY = "Technology"
-}
+import { Platform, Sector, Tag } from "./interfaces/product.interface";
 
 export abstract class ProductModel extends Model {
   public id!: string;
