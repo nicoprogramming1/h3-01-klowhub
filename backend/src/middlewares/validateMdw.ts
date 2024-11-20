30ed6c8eacbf0ecb75ec1508b2e6abac7766a16e
nicoprogramming <wnorowsky.nicolas@gmail.com>
2024-11-20 19:08:29 -0300

    Add login validator to mdw

--- backend/src/middlewares/validateMdw.ts
+++ backend/src/middlewares/validateMdw.ts
@@ -17,6 +17,19 @@ export const validateRegister: ValidationChain[] = [
     .withMessage('La contraseña debe tener al menos 6 caracteres'),
 ];
 
+export const validateLogin: ValidationChain[] = [
+  check('email')
+    .notEmpty()
+    .withMessage('El campo email es requerido')
+    .isEmail()
+    .withMessage('Debe proveer un email válido'),
+  check('password')
+    .notEmpty()
+    .withMessage('El campo password es requerido')
+    .isLength({ min: 6 })
+    .withMessage('La contraseña debe tener al menos 6 caracteres'),
+];
+
 export const handleValidationErrors = (
   req: Request,
   res: Response,
