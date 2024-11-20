ddbc056998e7dd52a6f9bee6bccbd5cef0a57575
nicoprogramming <wnorowsky.nicolas@gmail.com>
2024-11-20 19:08:20 -0300

    Add mdw to routes

--- backend/src/routes/authRoutes.ts
+++ backend/src/routes/authRoutes.ts
@@ -1,12 +1,12 @@
 import { Router } from 'express';
 import { register, login, logout } from '../controllers/authController';
-import { validateRegister, handleValidationErrors } from '../middlewares/validateMdw';
+import { validateRegister, validateLogin, handleValidationErrors } from '../middlewares';
 
 const routerAuth = Router();
 
 // Definir las rutas
 routerAuth.post('/register', validateRegister, handleValidationErrors, register);
-routerAuth.post('/login', login);
+routerAuth.post('/login', validateLogin, handleValidationErrors, login);
 routerAuth.delete('/logout', logout);
 
 export default routerAuth;
