ac856f7dc93bb10814af06f07a8fe5913c1b1388
nicoprogramming <wnorowsky.nicolas@gmail.com>
2024-11-22 11:34:24 -0300

    Add authenticate to course register

--- backend/src/routes/courseRoutes.ts
+++ backend/src/routes/courseRoutes.ts
@@ -1,9 +1,16 @@
-import { Router } from "express";
-import { createCourse } from "../controllers/courseController";
-import {validateCourseRegistration, handleValidationErrors} from '../utils/middlewares'
+import { Router } from 'express';
+import { createCourse } from '../controllers/courseController';
+import {
+  validateCourseRegistration,
+  handleValidationErrors,
+} from '../utils/middlewares';
+import passport from 'passport';
 
 const courseRouter = Router();
+const authenticate = passport.authenticate('jwt', { session: false })
 
-courseRouter.post("/", validateCourseRegistration, handleValidationErrors, createCourse);
+courseRouter
+  .route('/')
+  .post(authenticate, validateCourseRegistration, handleValidationErrors, createCourse);
 
 export default courseRouter;
