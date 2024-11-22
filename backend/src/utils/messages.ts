eaafedc36cfe398bfeacdc7d6b271dac759a4613
nicoprogramming <wnorowsky.nicolas@gmail.com>
2024-11-22 13:22:56 -0300

    Add messages const

---
+++ backend/src/utils/messages.ts
@@ -0,0 +1,11 @@
+export const MESSAGES = {
+    COURSE_NOT_FOUND: "El curso no existe",
+    CREATE_SUCCESS: "La operación de registro fue un éxito",
+    CREATE_ERROR: "Error durante la operación de registro",
+    FETCH_SUCCESS: "La información solicitada fue recuperada con éxito",
+    FETCH_ERROR: "Error al solicitar la información",
+    SERVER_ERROR: "Error de servidor al ejecutar la operación",
+    CONNECTION_ERROR: "Error en la conexión con la base de datos",
+    MISSED_DATA: "La información brindada no es suficiente",
+  };
+  
