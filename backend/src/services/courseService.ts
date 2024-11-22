66afb417909a8331b0241f78b75078707b267221
nicoprogramming <wnorowsky.nicolas@gmail.com>
2024-11-22 13:22:49 -0300

    Add getCourses in courseService

--- backend/src/services/courseService.ts
+++ backend/src/services/courseService.ts
@@ -1,10 +1,10 @@
-import { Transaction } from "sequelize";
-import sequelize from "../config/database";
-import CourseModel from "../models/CourseModel";
-import CourseModuleModel from "../models/CourseModuleModel";
-import LessonModel from "../models/LessonModel";
-import { CourseData } from "../models/interfaces/course.interface";
-
+import { Transaction } from 'sequelize';
+import sequelize from '../config/database';
+import CourseModel from '../models/CourseModel';
+import CourseModuleModel from '../models/CourseModuleModel';
+import LessonModel from '../models/LessonModel';
+import { CourseData } from '../models/interfaces/course.interface';
+import { MESSAGES } from '../utils/messages';
 
 const saveCourse = async (courseData: CourseData) => {
   const transaction: Transaction = await sequelize.transaction();
@@ -35,11 +35,56 @@ const saveCourse = async (courseData: CourseData) => {
     await transaction.commit();
 
     return course;
-  } catch (error) {
-    // Revertir cambios en caso de error
+  } catch (error: any) {
     await transaction.rollback();
-    throw new Error(`Error al crear el curso: ${error}`);
+    if (error.name === 'SequelizeConnectionError') {
+      throw new Error(MESSAGES.CONNECTION_ERROR);
+    }
+    throw new Error(`${MESSAGES.FETCH_ERROR} | ${error.message}`);
   }
 };
 
-export default { saveCourse };
+const findCourse = async (courseId: string) => {
+  try {
+    const course = await CourseModel.findOne({
+      where: { id: courseId },
+      attributes: [
+        'title',
+        'detail',
+        'aboutLearn',
+        'competence',
+        'platform',
+        'imageMain',
+        'sector',
+        'tags',
+        'price',
+      ], // Solo devuelve estos campos del curso
+      include: [
+        {
+          model: CourseModuleModel,
+          attributes: ['title', 'detail'],
+          include: [
+            {
+              model: LessonModel,
+              attributes: [
+                'title',
+                'detail',
+                'lessonLink',
+                'additionalPdf1',
+                'additionalPdf2',
+              ],
+            },
+          ],
+        },
+      ],
+    });
+    return course;
+  } catch (error: any) {
+    if (error.name === 'SequelizeConnectionError') {
+      throw new Error(MESSAGES.CONNECTION_ERROR);
+    }
+    throw new Error(`${MESSAGES.FETCH_ERROR} | ${error.message}`);
+  }
+};
+
+export default { saveCourse, findCourse };
