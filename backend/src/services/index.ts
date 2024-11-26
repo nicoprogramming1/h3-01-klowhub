4ac3ed8028ed55859df2d4f9116058a07b1c6029
wnorowsky <wnorowsky.nicolas@gmail.com>
2024-11-26 18:56:42 -0300

    Add userService

--- backend/src/services/index.ts
+++ backend/src/services/index.ts
@@ -1,4 +1,5 @@
 import * as courseService from "./course.service";
 import * as auth from "./auth.service";
+import * as userService from "./user.service";
 
-export { auth, courseService };
+export { auth, courseService, userService };
