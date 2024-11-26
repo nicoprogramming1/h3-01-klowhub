68fb91d02675388a022962714cfb55d2effb77f3
wnorowsky <wnorowsky.nicolas@gmail.com>
2024-11-26 18:55:07 -0300

    Add messages.ts

--- backend/src/utils/messages.ts
+++ backend/src/utils/messages.ts
@@ -1,11 +1,15 @@
 export const MESSAGES = {
-    COURSE_NOT_FOUND: "El curso no existe",
-    CREATE_SUCCESS: "La operación de registro fue un éxito",
-    CREATE_ERROR: "Error durante la operación de registro",
-    FETCH_SUCCESS: "La información solicitada fue recuperada con éxito",
-    FETCH_ERROR: "Error al recuperar la información",
-    SERVER_ERROR: "Error de servidor al ejecutar la operación",
-    CONNECTION_ERROR: "Error en la conexión con la base de datos",
-    MISSED_DATA: "La información brindada no es suficiente",
-  };
-  
+  CONNECTION_ERROR: "Error en la conexión con la base de datos",
+  COURSE_NOT_FOUND: "El curso no existe",
+  CREATE_SUCCESS: "La operación de registro fue un éxito",
+  CREATE_ERROR: "Error durante la operación de registro",
+  ELIMINATE_SUCCESS: "La eliminación se completó con éxito",
+  ELIMINATE_ERROR: "Error durante la eliminación",
+  FETCH_SUCCESS: "La información solicitada fue recuperada con éxito",
+  FETCH_ERROR: "Error al recuperar la información",
+  MISSED_DATA: "La información brindada no es suficiente",
+  SERVER_ERROR: "Error de servidor al ejecutar la operación",
+  UPDATE_ERROR: "Error durante la actualización",
+  UPDATE_SUCCESS: "La actualización se completó con éxito",
+  USER_NOT_FOUND: "El usuario no existe",
+};
