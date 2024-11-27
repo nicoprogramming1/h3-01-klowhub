42737c6a4b8706a400e21bba4e21bfbabc4c75db
wnorowsky <wnorowsky.nicolas@gmail.com>
2024-11-27 10:42:45 -0300

    Fix user controllers

--- backend/src/controllers/user.controller.ts
+++ backend/src/controllers/user.controller.ts
@@ -1,94 +1,66 @@
 import { Request, Response } from "express";
 import { userService } from "../services";
 import { MESSAGES } from "../utils/messages";
-import { UserDTO } from "../dtos/user.dto";
 
 export const updateUser = async (req: Request, res: Response) => {
   try {
     const { id } = req.params;
     const { password, ...updateData } = req.body;
 
-    const updatedUser = await userService.updateUserById(
-      id,
-      updateData,
-      password
-    );
-
-    // DTO para no exponer datos sensibles como la pass
-    const userDTO: UserDTO = {
-      longName: updatedUser.longName,
-      email: updatedUser.email,
-      country: updatedUser.country,
-      imageProfile: updatedUser.imageProfile,
-    };
+    const updatedUser = await userService.updateUserById(id, updateData, password);
 
     res.status(200).json({
-      user: userDTO,
+      user: updatedUser,
       message: MESSAGES.UPDATE_SUCCESS,
     });
   } catch (error: any) {
-    console.error(MESSAGES.UPDATE_ERROR, error.message);
-    if (error.message === MESSAGES.USER_NOT_FOUND) {
-      res.status(404).json({ message: error.message });
-    } else {
-      res.status(500).json({ message: MESSAGES.UPDATE_ERROR });
+    if (res.headersSent) {
+      return; // Si los encabezados ya se enviaron, no hacer nada más
     }
+    console.error(MESSAGES.UPDATE_ERROR, error.message);
+    res.status(error.message === MESSAGES.USER_NOT_FOUND ? 404 : 500).json({ message: error.message });
   }
 };
+
 
 //Llamar a un usuario por id
 export const getOneUser = async (req: Request, res: Response) => {
   try {
     const id = req.params.id;
 
-    const userDeactivated = await userService.findUserByPk(id);
-    if (!userDeactivated) {
+    const user = await userService.findUserDTOByPk(id);
+    if (!user) {
       res.status(404).json({ message: MESSAGES.USER_NOT_FOUND });
       return;
     }
 
-    const userDTO: UserDTO = {
-      longName: userDeactivated.longName,
-      email: userDeactivated.email,
-      country: userDeactivated.country,
-      imageProfile: userDeactivated.imageProfile,
-    };
-
-    res.status(200).json({ user: userDTO, message: MESSAGES.FETCH_SUCCESS });
+    res.status(200).json({ user, message: MESSAGES.FETCH_SUCCESS });
   } catch (error: any) {
-    console.error(MESSAGES.UPDATE_ERROR, error.message);
-    if (error.message === MESSAGES.USER_NOT_FOUND) {
-      res.status(404).json({ message: error.message });
-    } else {
-      res.status(500).json({ message: MESSAGES.UPDATE_ERROR });
+    if (res.headersSent) {
+      return; // Si los encabezados ya se enviaron, no hacer nada más
     }
+    console.error(MESSAGES.FETCH_ERROR, error.message);
+    res.status(500).json({ message: MESSAGES.FETCH_ERROR });
   }
 };
+
 
 //Desactivar la cuenta del usuario
 export const deactivateUser = async (req: Request, res: Response) => {
   try {
     const { id } = req.params;
 
-    const userDeactivated = await userService.deactivateUserByPk(id);
-
-    const userDTO: UserDTO = {
-        longName: userDeactivated.longName,
-        email: userDeactivated.email,
-        country: userDeactivated.country,
-        imageProfile: userDeactivated.imageProfile,
-      };
+    const deactivatedUser = await userService.deactivateUserByPk(id);
 
     res.status(200).json({
       message: MESSAGES.ELIMINATE_SUCCESS,
-      user: userDTO
+      user: deactivatedUser,
     });
   } catch (error: any) {
-    console.error(MESSAGES.UPDATE_ERROR, error.message);
-    if (error.message === MESSAGES.USER_NOT_FOUND) {
-      res.status(404).json({ message: error.message });
-    } else {
-      res.status(500).json({ message: MESSAGES.ELIMINATE_ERROR });
+    if (res.headersSent) {
+      return; // Si los encabezados ya se enviaron, no hacer nada más
     }
+    console.error(MESSAGES.ELIMINATE_ERROR, error.message);
+    res.status(error.message === MESSAGES.USER_NOT_FOUND ? 404 : 500).json({ message: error.message });
   }
 };
