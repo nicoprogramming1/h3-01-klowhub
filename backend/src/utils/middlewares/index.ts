00fe6222c63bd5108ae3c67cd7d0bc81b7bda54e
wnorowsky <wnorowsky.nicolas@gmail.com>
2024-11-27 10:41:26 -0300

    Add validatemdw

--- backend/src/utils/middlewares/index.ts
+++ backend/src/utils/middlewares/index.ts
@@ -2,6 +2,14 @@ export {
   validateRegister,
   validateLogin,
   validateLogout,
-} from './validateAuthMdw';
-export { handleValidationErrors } from './handleValidator';
-export { validateCourseRegistration, validateFetchCourse } from './validateCourseMdw';
+} from "./validateAuthMdw";
+export { handleValidationErrors } from "./handleValidator";
+export {
+  validateCourseRegistration,
+  validateFetchCourse,
+} from "./validateCourseMdw";
+export {
+  deactivateUserValidator,
+  getOneUserValidator,
+  updateUserValidator
+} from "./validateUser";
