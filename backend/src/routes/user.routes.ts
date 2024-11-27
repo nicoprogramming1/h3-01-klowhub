38306bc209d3c4b1361d9ceb3ef1dac2bec3a6bb
wnorowsky <wnorowsky.nicolas@gmail.com>
2024-11-27 10:41:54 -0300

    Add validations

--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -1,13 +1,14 @@
 import passport from "passport";
 import express from 'express';
 import { getOneUser, deactivateUser, updateUser } from '../controllers/user.controller'
+import { deactivateUserValidator, getOneUserValidator, handleValidationErrors, updateUserValidator } from '../utils/middlewares'
 
 const userRouter = express.Router();
 const authenticate = passport.authenticate('jwt', { session: false });
 
 userRouter.route('/:id')
-    .get(authenticate, getOneUser)
-    .delete(authenticate, deactivateUser)
-    .put(authenticate, updateUser);
+    .get(authenticate, getOneUserValidator, handleValidationErrors, getOneUser)
+    .delete(authenticate, deactivateUserValidator, handleValidationErrors, deactivateUser)
+    .put(authenticate, updateUserValidator, handleValidationErrors, updateUser);
 
 export default userRouter;
