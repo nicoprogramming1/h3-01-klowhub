22f624f11f8669949ebffd1344b7dc621bc1469d
wnorowsky <wnorowsky.nicolas@gmail.com>
2024-11-29 18:47:10 -0300

    Add userPro dto

--- backend/src/dtos/user.dto.ts
+++ backend/src/dtos/user.dto.ts
@@ -1,7 +1,26 @@
+import { Membership, PaymentMethod, Sector, Tool } from "../models/enum/enum";
+
 export interface UserDTO {
   longName: string;
   email: string;
+}
+
+export interface UserProDTO {
+  firstName: string;
+  lastName: string;
+  description?: string;
   country?: string;
-  imageProfile?: string;
-  /* phone: number; */
+  sector: Sector[];
+  sectorsExperience?: string;
+  tools: Tool[];
+  toolsExperience?: string;
+  portfolioLink?: string;
+  academicFormation?: string;
+  certificationLink?: string;
+  certificationFiles?: string[];
+  PaymentMethod: PaymentMethod;
+  accountData: string;
+  imageProfile?: string
+  membership: Membership;
+  isMentor?: boolean  // si quiere ser mentor el front enviara un true, sino no lo envia
 }
