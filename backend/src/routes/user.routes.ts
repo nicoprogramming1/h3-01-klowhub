fab701a5de20667c2218b383ca99bbff094a04ed
wnorowsky <wnorowsky.nicolas@gmail.com>
2024-11-30 11:39:10 -0300

    Remove membership and userPro routes

--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -1,18 +1,15 @@
 import passport from "passport";
 import express from 'express';
-import { getOneUser, deactivateUser, updateUser, getUserMembership, changeMembership } from '../controllers/user.controller'
-import { changeMembershipValidator, idByParameterValidator, handleValidationErrors, updateUserValidator } from '../middlewares'
+import { getOneUser, deactivateUser, updateUser } from '../controllers/user.controller'
+import { idByParameterValidator, handleValidationErrors, updateUserValidator } from '../middlewares'
 
 const userRouter = express.Router();
 const authenticate = passport.authenticate('jwt', { session: false });
 
+// User básico
 userRouter.route('/:id')
     .get(authenticate, idByParameterValidator, handleValidationErrors, getOneUser)
     .delete(authenticate, idByParameterValidator, handleValidationErrors, deactivateUser)
     .put(authenticate, updateUserValidator, handleValidationErrors, updateUser);
-
-userRouter.route('/membership/:id')
-    .get(authenticate, idByParameterValidator, handleValidationErrors, getUserMembership)
-    .put(authenticate, idByParameterValidator, changeMembershipValidator, handleValidationErrors, changeMembership)
 
 export default userRouter;
