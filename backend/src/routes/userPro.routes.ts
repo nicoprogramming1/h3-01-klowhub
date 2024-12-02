00469a3310a4ee9b2ce5349b027d959f5dd8d559
wnorowsky <wnorowsky.nicolas@gmail.com>
2024-12-02 20:14:55 -0300

    Changed cloduinary and multer config

--- backend/src/routes/userPro.routes.ts
+++ backend/src/routes/userPro.routes.ts
@@ -1,15 +1,17 @@
 import passport from "passport";
 import express from 'express';
-import { getUserProByUserId, registerUserPro } from '../controllers/user.controller'
-import { idByParameterValidator, handleValidationErrors } from '../middlewares'
+import { getUserProByUserId, registerUserPro, updateUserPro } from '../controllers/userPro.controller'
+import { idByParameterValidator, handleValidationErrors, validateUserPro, updateUserValidator } from '../middlewares'
+import { uploadImageMdw } from '../middlewares'
 
 const userRouter = express.Router();
 const authenticate = passport.authenticate('jwt', { session: false });
+const multerMdw = uploadImageMdw.single('imageProfile')    // mdw de carga de imagen
 
 // User PRO -> el id es del user básico asociado a su perfil PRO
 userRouter.route('/:id')
-    .post(authenticate, idByParameterValidator, handleValidationErrors, registerUserPro)
+    .post(authenticate, idByParameterValidator, validateUserPro, uploadImageMdw.single('imageProfile'), handleValidationErrors, registerUserPro)
     .get(authenticate, idByParameterValidator, handleValidationErrors, getUserProByUserId)
-    /* .put(authenticate, idByParameterValidator, changeMembershipValidator, handleValidationErrors, ) */
+    .patch(authenticate, idByParameterValidator, updateUserValidator, handleValidationErrors, updateUserPro)
 
 export default userRouter;
