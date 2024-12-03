06bfc100e30d76064a0e69f6f56133b1df5582e9
wnorowsky <wnorowsky.nicolas@gmail.com>
2024-12-03 19:44:28 -0300

    Fix saveCourse service

--- backend/src/services/course.service.ts
+++ backend/src/services/course.service.ts
@@ -2,43 +2,53 @@ import { Transaction } from "sequelize";
 import sequelize from "../config/database";
 import { CourseModel, CourseModuleModel, LessonModel } from "../models/";
 import { MESSAGES } from "../utils/messages";
-import { CourseDTO } from "../dtos/course.dto";
+import { CourseDTO, LessonDataDTO, ModuleDataDTO } from "../dtos/course.dto";
 
-const saveCourse = async (courseData: CourseDTO) => {
+const saveCourse = async (courseData: CourseDTO): Promise<CourseDTO | null> => {
   const transaction: Transaction = await sequelize.transaction();
 
   try {
     const course = await CourseModel.create(courseData.course, { transaction });
 
+    const modulesWithLessons = [];
+
+    // Crear los módulos y sus lecciones asociadas
     for (const moduleData of courseData.modules) {
       const module = await CourseModuleModel.create(
         {
           ...moduleData,
-          courseId: course.id,
+          courseId: course.id, // Relacionar el módulo con el curso
         },
         { transaction }
       );
 
+      const lessons = [];
       for (const lessonData of moduleData.lessons) {
-        await LessonModel.create(
+        const lesson = await LessonModel.create(
           {
             ...lessonData,
-            courseModuleId: module.id,
+            courseModuleId: module.id, // Relacionar la lección con el módulo
           },
           { transaction }
         );
+        lessons.push(lesson); // Agregar lección a la lista
       }
+
+      modulesWithLessons.push({ ...module.toJSON(), lessons }); // Relacionar módulo con sus lecciones
     }
 
     await transaction.commit();
 
-    return course;
+    return {
+      course: course.toJSON(),
+      modules: modulesWithLessons,
+    };
   } catch (error: any) {
     await transaction.rollback();
     if (error.name === "SequelizeConnectionError") {
       throw new Error(MESSAGES.CONNECTION_ERROR);
     }
-    throw new Error(`${MESSAGES.FETCH_ERROR} | ${error.message}`);
+    throw new Error(`${MESSAGES.CREATE_ERROR} | ${error.message}`);
   }
 };
 
