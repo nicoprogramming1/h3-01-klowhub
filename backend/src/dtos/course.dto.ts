ebb152120d0cb0a6e8754cefc1a3777af7c491e5
wnorowsky <wnorowsky.nicolas@gmail.com>
2024-12-03 19:51:40 -0300

    Add id? to course, module and lesson

--- backend/src/dtos/course.dto.ts
+++ backend/src/dtos/course.dto.ts
@@ -2,6 +2,7 @@ import { Platform, Sector, Tag } from "../models/enum/enum";
 
 export interface CourseDTO {
     course: {
+      id?: string,
       title: string;
       detail: string;
       aboutLearn?: string;
@@ -16,12 +17,14 @@ export interface CourseDTO {
   }
   
   export interface ModuleDataDTO {
+    id?: string,
     title: string;
     detail: string;
     lessons: LessonDataDTO[];
   }
 
   export interface LessonDataDTO {
+    id?: string,
     title: string;
     detail: string;
     lessonLink: string;
