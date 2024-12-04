baa4cf3384089c812c359892e4fe42d45c4211fe
wnorowsky <wnorowsky.nicolas@gmail.com>
2024-12-04 11:20:37 -0300

    Add mdw to routes

--- backend/src/routes/course.routes.ts
+++ backend/src/routes/course.routes.ts
@@ -4,11 +4,15 @@ import {
   validateCourseRegistration,
   handleValidationErrors,
   validateFetchCourse,
+  uploadImageMdw,
+  idByParameterValidator
 } from '../middlewares';
+import { imageProController } from "../controllers";
 import passport from 'passport';
 
 const courseRouter = Router();
 const authenticate = passport.authenticate('jwt', { session: false });
+const multerMdw = uploadImageMdw.single('imageProfile')    // mdw de carga de imagen
 
 courseRouter
   .route('/')
@@ -19,8 +23,14 @@ courseRouter
     createCourse
   );
 
+courseRouter.route('/imageCourse/:id')
+  .post(authenticate, idByParameterValidator, multerMdw, handleValidationErrors, imageProController.imageRegisterCourse)
+
+courseRouter.route('/imageLesson/:id')
+  .post(authenticate, idByParameterValidator, multerMdw, handleValidationErrors, imageProController.imageRegisterLesson)
+
 courseRouter
   .route('/:id')
-  .get(validateFetchCourse, handleValidationErrors, getOneCourse);
+  .get(idByParameterValidator, validateFetchCourse, handleValidationErrors, getOneCourse);
 
 export default courseRouter;
