39084c910c7b4c8971b799aef815f7b284dfd0af
wnorowsky <wnorowsky.nicolas@gmail.com>
2024-12-04 13:50:25 -0300

    rename image controller

--- backend/src/controllers/index.ts
+++ backend/src/controllers/index.ts
@@ -2,6 +2,6 @@ import * as authController from './auth.controller';
 import * as userController from './user.controller';
 import * as courseController from './course.controller';
 import * as userProController from './userPro.controller';
-import * as imageProController from './image.controller';
+import * as imageController from './image.controller';
 
-export { authController, userController, courseController, userProController, imageProController };
+export { authController, userController, courseController, userProController, imageController };
