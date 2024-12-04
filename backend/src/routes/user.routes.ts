09832a48e58d71b39ad5410218bc5c0d20d829ca
wnorowsky <wnorowsky.nicolas@gmail.com>
2024-12-04 13:50:51 -0300

    Add imageroute to userroutes

--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -1,15 +1,20 @@
 import passport from "passport";
 import express from 'express';
 import { getOneUser, deactivateUser, updateUser } from '../controllers/user.controller'
-import { idByParameterValidator, handleValidationErrors, updateUserValidator } from '../middlewares'
+import { idByParameterValidator, handleValidationErrors, updateUserValidator, uploadImageMdw } from '../middlewares'
+import { imageController } from "../controllers";
 
 const userRouter = express.Router();
 const authenticate = passport.authenticate('jwt', { session: false });
+const multerMdw = uploadImageMdw.single('imageProfile')    // mdw de carga de imagen
 
 // User básico
 userRouter.route('/:id')
     .get(authenticate, idByParameterValidator, handleValidationErrors, getOneUser)
     .delete(authenticate, idByParameterValidator, handleValidationErrors, deactivateUser)
-    .put(authenticate, updateUserValidator, handleValidationErrors, updateUser);
+    .patch(authenticate, updateUserValidator, handleValidationErrors, updateUser);
+
+userRouter.route('/imageProfile/:id')
+    .patch(authenticate, idByParameterValidator, multerMdw, handleValidationErrors, imageController.imageRegisterUser)
 
 export default userRouter;
