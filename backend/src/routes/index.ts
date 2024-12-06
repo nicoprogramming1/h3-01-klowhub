e9fbf7c88895ea9cd4277caa0e43058e594fde34
wnorowsky <wnorowsky.nicolas@gmail.com>
2024-12-06 13:18:08 -0300

    add port

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -4,6 +4,7 @@ import userRoutes from './user.routes';
 import courseRoutes from './course.routes';
 import membershipRoutes from './membership.routes'
 import userProRoutes from './userPro.routes'
+/* import oAuthrouter from './oAuth.routes'; */
 
 const router = Router();
 
@@ -12,5 +13,6 @@ router.use('/user', userRoutes);
 router.use('/course', courseRoutes);
 router.use('/membership', membershipRoutes);
 router.use('/userPro', userProRoutes);
+/* router.use('/googleUser', oAuthrouter); */
 
 export default router;
